d18cd6ed1e25152bdb472b1660634508351aa10a
Luca Novara <luca.n88@gmail.com>
2018-03-08 15:32:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+Ansible Role: SSH
+=========
+
+[![Build Status](https://travis-ci.org/lnovara/ansible-ssh.svg?branch=master)](https://travis-ci.org/lnovara/ansible-ssh)
+[![Ansible Galaxy](https://img.shields.io/badge/galaxy-lnovara.ssh-blue.svg)](https://galaxy.ansible.com/lnovara/ssh)
+
+Install and configure SSH server and client.
+
+Requirements
+------------
+
+An Ansible 2.2 or higher installation.
+
+Role Variables
+--------------
+
+Available variables are listed below, along with default values (see
+defaults/main.yml).
+
+    ssh_client_packages:
+      - openssh-client
+
+Packages to install to provide SSH client functionalities.
+
+    ssh_server_packages:
+      - openssh-server
+
+Packages to install to provide SSH server functionalities.
+
+    ssh_client_config: {}
+
+Dictionary holding SSH client configuration. The complete SSH client
+configuration reference can be found
+[here](https://linux.die.net/man/5/ssh_config).<br/>
+**NOTE**: the provided SSH client configuration will be merged with the default
+one defined in `vars/main.yml`.
+
+    ssh_server_config: {}
+
+Dictionary holding SSH server configuration. The complete SSH server
+configuration reference can be found
+[here](https://linux.die.net/man/5/sshd_config).<br/>
+**NOTE**: the provided SSH server configuration will be merged with the default
+one defined in `vars/main.yml`.
+
+    ssh_rebuild_moduli: yes
+
+Rebulild `/etc/ssh/moduli` file that contain primes for Diffie-Hellman Group
+Exchange (DH-GEX).
+
+    ssh_moduli_bits: 4096
+
+Number of bits for DH-GEX primes.
+
+    ssh_rebuild_host_keys: yes
+
+Rebuild SSH host keys.
+
+    ssh_host_keys_bits:
+      ed25519: 512  # Ed25519 keys have fixed length, number of bits is ignored
+      rsa: 4096
+
+Dictionary holding key type and length of SSH public keys to be created.
+
+Dependencies
+------------
+
+[config_encoder_filters](https://github.com/jtyr/ansible-config_encoder_filters)
+
+Example Playbook
+----------------
+
+    - name: Install and configure SSH on all hosts.
+      hosts: all
+      roles:
+         - config_encoder_filters
+         - lnovara.ssh
+
+Testing
+-------
+
+This role uses [molecule](https://molecule.readthedocs.io/en/latest/) to
+implement automatic testing of its functionalities.
+
+To execute the tests
+
+```bash
+pip install tox
+
+git clone https://github.com/lnovara/ansible-ssh.git
+
+cd ansible-ssh
+
+# test all the scenarios
+tox
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Luca Novara
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,97 @@
+import os
+import pytest
+import yaml
+
+import testinfra.utils.ansible_runner
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.fixture()
+def ansible_defaults():
+    with open('playbook-vars.yml', 'r') as stream:
+        return yaml.load(stream)
+
+
+@pytest.mark.parametrize('package', ansible_defaults()['ssh_client_packages'])
+def test_ssh_client_packages(host, package):
+    assert host.package(package).is_installed
+
+
+@pytest.mark.parametrize('package', ansible_defaults()['ssh_server_packages'])
+def test_ssh_server_packages(host, package):
+    assert host.package(package).is_installed
+
+
+def test_ssh_moduli(host):
+    f = host.file('/etc/ssh/moduli')
+
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
+
+    assert not host.file('/etc/ssh/moduli.all').exists
+    assert not host.file('/etc/ssh/moduli.safe').exists
+
+
+@pytest.mark.parametrize('ssh_key',
+                         ansible_defaults()['ssh_host_keys_bits'].keys())
+def test_ssh_keys(host, ssh_key):
+    priv = host.file('/etc/ssh/ssh_host_%s_key' % ssh_key)
+
+    assert priv.exists
+    assert priv.is_file
+    assert priv.user == 'root'
+    assert priv.group == 'root'
+    assert priv.mode == 0o600
+
+    pub = host.file('/etc/ssh/ssh_host_%s_key.pub' % ssh_key)
+
+    assert pub.exists
+    assert pub.is_file
+    assert pub.user == 'root'
+    assert pub.group == 'root'
+    assert pub.mode == 0o644
+
+    conf = host.file('/etc/ssh/sshd_config')
+
+    assert conf.contains('HostKeyAlgorithms %s' % ssh_key)
+    assert conf.contains('HostKey /etc/ssh/ssh_host_%s_key' % ssh_key)
+
+
+@pytest.mark.parametrize('opt,value',
+                         ansible_defaults()['ssh_client_config'].items())
+def test_ssh_client_config(host, opt, value):
+    f = host.file('/etc/ssh/ssh_config')
+
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
+
+    assert f.contains('%s %s' % (opt, value))
+
+
+@pytest.mark.parametrize('opt,value',
+                         ansible_defaults()['ssh_server_config'].items())
+def test_ssh_server_config(host, opt, value):
+    f = host.file('/etc/ssh/sshd_config')
+
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
+
+    assert f.contains('%s %s' % (opt, value))
+
+
+def test_ssh_service(host):
+    s = host.ansible('service', 'name=ssh state=started enabled=yes')
+
+    assert not s['changed']
